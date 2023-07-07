ad2ecad47cf0792b1dc9fafaff3afefacdddcfc7
DFERNANDEZ <dfernandez@nacionservicios.com.ar>
2023-07-07 01:09:06 -0300

    cambios sobre segunda pre entrega

--- src/routes/carts.router.js
+++ src/routes/carts.router.js
@@ -1,6 +1,7 @@
 import { Router } from "express";
 import CartManager from '../dao/managers/carts.manager.js'
 import ProductManager from "../dao/managers/products.manager.js";
+import { cartModel } from "../dao/models/cart.model.js";
 
 const router = Router();
 
@@ -29,7 +30,7 @@ router.get("/:cid", async (req, res) => {
 router.post("/:cid/products/:pid", async (req, res) => {
     try {
         let { cid, pid } = req.params;
-        const cart = await cartManager.getCartById(cid);
+        const cart = await cartManager.getCartById(cid);        
         const product = await productManager.getProductById(pid);
 
         const carts = await cartManager.addProductToCart(cart, product);
@@ -38,5 +39,64 @@ router.post("/:cid/products/:pid", async (req, res) => {
         res.status(500).send({ status: "error", payload: err.message });
     }
 });
+
+router.delete("/:cid/products/:pid", async (req, res) => {
+    try {
+        let { cid, pid } = req.params;
+        const cart = await cartManager.getCartById(cid);
+        const product = await productManager.getProductById(pid);
+
+        const carts = await cartManager.deleteProductFromCart(cart, product);
+        res.status(201).send({ status: "success", payload: carts });
+    } catch (err) {
+        res.status(500).send({ status: "error", payload: err.message });
+    }
+});
+
+router.delete("/:cid", async (req, res) => {
+    try {
+        const { cid } = req.params;
+
+        const cart = await cartManager.deleteAllProductsFromCart(cid);
+
+        res.status(200).json({ status: "success", payload: cart });
+    } catch (error) {
+        res.status(500).json({ status: "error", payload: error.message });
+    }
+});
+
+router.put('/:cid', async (req, res) => {
+    try {
+        const { cid } = req.params;
+        const { products } = req.body;
+
+        const cart = await cartModel.findByIdAndUpdate(cid, { products }, { new: true });
+        if (!cart) {
+            return res.status(404).json({ status: 'error', payload: 'No existe el carrito' });
+        }
+        
+        const response = {
+        status: 'success',
+        payload: cart.products
+        };
+
+        res.json(response);
+    } catch (error) {
+        res.status(500).json({ status: 'error', payload: error.message });
+    }
+});
+
+router.put('/:cid/products/:pid', async (req, res) => {
+    try {
+        const { cid, pid } = req.params;
+        const { quantity } = req.body;
+    
+        const updatedCart = await cartManager.updateProductQuantity(cid, pid, quantity);
+    
+        res.status(200).json({ status: "success", payload: updatedCart.products });
+    } catch (error) {
+        res.status(500).json({ status: "error", payload: error.message });
+    }
+    });
 
 export default router;
--- src/routes/views.router.js
+++ src/routes/views.router.js
@@ -1,16 +1,87 @@
-import { Router } from 'express';
-import ProductManager from '../dao/managers/ProductManager.js';
+import {
+    Router
+} from 'express';
+import ProductManager from '../dao/managers/products.manager.js';
 import MessageManager from '../dao/managers/message.manager.js';
+import {
+    productModel
+} from "../dao/models/product.model.js";
+import CartManager from '../dao/managers/carts.manager.js'
 
 const router = Router();
 const productManager = new ProductManager();
+const cartManager = new CartManager();
 const messageManager = new MessageManager();
 
-router.get('/', async (req, res) => {
-    const product = await productManager.getProducts();
-    res.render('index', {
-        product
-    });
+router.get('/products', async (req, res) => {
+    let {
+        limit = 3, page = 1, sort, query, category, availability
+    } = req.query;
+    limit = parseInt(limit);
+    page = parseInt(page);
+
+    const options = {
+        page,
+        limit,
+        sort: sort === 'desc' ? '-price' : 'price',
+        lean: true
+    };
+
+    const filter = {};
+
+    if (query) {
+        filter.title = {
+            $regex: query,
+            $options: 'i'
+        };
+    }
+    if (category && category !== 'undefined') {
+        filter.category = category;
+    }
+    if (availability) {
+        if (availability === 'available') {
+            filter.stock = {
+                $gt: 0
+            };
+        } else if (availability === 'out-of-stock') {
+            filter.stock = 0;
+        }
+    }
+
+    let result = await productModel.paginate(filter, options);
+
+    result.prevLink = result.hasPrevPage ? `http://localhost:8080/products?page=${result.prevPage}` : '';
+    result.nextLink = result.hasNextPage ? `http://localhost:8080/products?page=${result.nextPage}` : '';
+    result.isValid = !(page <= 0 || page > result.totalPages);
+    console.log(result)
+    res.render('products', result);
+});
+
+router.get('/carts/:cid', async (req, res) => {
+    try {
+        const {
+            cid
+        } = req.params;
+        const cart = await cartManager.getCartById(cid);
+
+        const products = await Promise.all(
+            cart.products.map(async (product) => {
+                const productId = product.productId;
+                const productData = await productModel.findById(productId);
+                return productData.toObject();
+            })
+        );
+
+        res.render('cart', {
+            cart: cart.toObject(),
+            products
+        });
+    } catch (err) {
+        res.status(500).send({
+            status: 'error',
+            payload: err.message
+        });
+    }
 });
 
 router.get('/realtimeproducts', async (req, res) => {
@@ -23,18 +94,26 @@ router.get('/realtimeproducts', async (req, res) => {
 router.get('/chat', async (req, res) => {
     const messages = await messageManager.getMessages();
     const simplifiedMessages = messages.map(message => message.toObject());
-    res.render('chat', { messages: simplifiedMessages });
+    res.render('chat', {
+        messages: simplifiedMessages
+    });
 });
 
 router.post('/api/messages', async (req, res) => {
-    const { user, message } = req.body;
+    const {
+        user,
+        message
+    } = req.body;
     const newMessage = await messageManager.addMessage(user, message);
-    
+
     // Obtener el índice del nuevo mensaje
     const messages = await messageManager.getMessages();
     const index = messages.findIndex(m => m._id.toString() === newMessage._id.toString());
 
-    res.json({ ...newMessage.toObject(), index });
+    res.json({
+        ...newMessage.toObject(),
+        index
+    });
 });
 
 export default router;
